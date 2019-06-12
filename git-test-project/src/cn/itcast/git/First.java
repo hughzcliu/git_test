d1d95830d8f40fd32de33612b75487f12e9f2ff0
hughzcl <hughzcl@itcast.cn>
2019-06-12 20:03:07 +0800

    把本地工程添加到git

---
+++ git-test-project/src/cn/itcast/git/First.java
@@ -0,0 +1,15 @@
+package cn.itcast.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
